c2d59f170c3e491f6666399a9e62817926d3327a
xupengfei <faberxu@gmail.com>
2022-08-31 16:44:04 +0800

    0.0.5 update to react18

--- packages/fa-cron-react-editor/src/main.tsx
+++ packages/fa-cron-react-editor/src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
+
+// 新的 createRoot API
+const container = document.getElementById('root');
+
+// @ts-ignore
+const root = createRoot(container);
+root.render(<App />);
