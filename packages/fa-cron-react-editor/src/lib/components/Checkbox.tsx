a03f5eec1f6f017e4199be2639e4772eb36d3e41
xupengfei <faberxu@gmail.com>
2022-09-01 11:06:55 +0800

    update checkbox style

--- packages/fa-cron-react-editor/src/lib/components/Checkbox.tsx
+++ packages/fa-cron-react-editor/src/lib/components/Checkbox.tsx
@@ -7,9 +7,9 @@ export default function Checkbox({ label, checked, onChange, style }: CheckboxPr
     const [id] = useState<string>(generateId())
 
     return (
-        <div style={style} className='fa-cron-react-editor-flex-row-center'>
-            <input id={id} type="checkbox" checked={checked} onChange={(e) => { onChange(!checked) }}></input>
-            <label htmlFor={id}>{label}</label>
+        <div style={style} className='fa-cron-react-editor-checkbox' onClick={() => onChange(!checked)}>
+            <i className={checked ? 'fa-cron-react-editor-checkbox-icon-ok' : 'fa-cron-react-editor-checkbox-icon-not'} />
+            <span>{label}</span>
         </div>
     )
 };
